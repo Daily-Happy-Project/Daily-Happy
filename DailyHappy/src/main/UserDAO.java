848414dfbf675b6c89fdb5d06a4494f89ebdb195
princesssuna <1009suna@gmail.com>
2021-11-09 00:03:00 +0900

    UserDAO 로그인 추가

--- DailyHappy/src/main/UserDAO.java
+++ DailyHappy/src/main/UserDAO.java
@@ -61,4 +61,21 @@ public class UserDAO {
             
         }
     }
+    
+    public int login(String uid, String upass) throws NamingException, SQLException { Connection conn = null;
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
+    try {
+    String sql = "SELECT id, password FROM user WHERE id = ?";
+    conn = ConnectionPool.get();
+    stmt = conn.prepareStatement(sql); stmt.setString(1, uid);
+    rs = stmt.executeQuery();
+    if (!rs.next()) return 1;
+    if (!upass.equals(rs.getString("password"))) return 2; return 0;
+    } finally {
+    if(rs!=null) rs.close();
+    if(stmt!=null) stmt.close();
+    if(conn!=null) conn.close();
+    }
+    }
 }
