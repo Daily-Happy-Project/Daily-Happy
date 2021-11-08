84b88bb3e8e3fd616252b6a074c2edba6c7d440b
princesssuna <1009suna@gmail.com>
2021-11-08 23:25:32 +0900

    커넥션 풀

---
+++ DailyHappy/src/util/ConnectionPool.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.*;
+
+import javax.naming.*;
+import javax.sql.DataSource;
+
+public class ConnectionPool {
+	private static DataSource _ds = null;
+	public static Connection get() throws NamingException, SQLException {
+	if (_ds == null) {
+	_ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/mysns");
+	}
+	return _ds.getConnection(); }
+}
