e6f3177d6615eae3110dccfcf870cdbc16be8413
princesssuna <1009suna@gmail.com>
2021-11-13 00:44:08 +0900

    context.xml jdbc 변경

--- DailyHappy/src/main/java/dto/UserDTO.java
+++ DailyHappy/src/main/java/dto/UserDTO.java
@@ -7,6 +7,7 @@ public class UserDTO {
 	private String name;
 	private String pw;
 	private int coin;
+	private int userThemeCode;
 	private Date join_date;
 	
 	public int getCoin() { //코인 얻음 
@@ -33,6 +34,13 @@ public class UserDTO {
 	public void setPw(String pw) {
 		this.pw=pw;
 	}
+	public int getUserThemeCode() {
+		return userThemeCode;
+	}
+	public void setUserThemeCode(int userThemeCode) {
+		this.userThemeCode=userThemeCode;
+	}
+	
 	public void setJoin_date(Date join_date) {
 		this.join_date=join_date;
 	}
