9ec252464028814eceed6f378b11bfbe21f262db
princesssuna <1009suna@gmail.com>
2021-11-13 00:46:44 +0900

    ConnectionPool 변경

--- DailyHappy/src/main/java/util/ConnectionPool.java
+++ DailyHappy/src/main/java/util/ConnectionPool.java
@@ -9,7 +9,7 @@ public class ConnectionPool {
 	private static DataSource _ds = null;
 	public static Connection get() throws NamingException, SQLException {
 	if (_ds == null) {
-	_ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/mysns");
+	_ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/daily_happy");
 	}
 	return _ds.getConnection(); }
 }
