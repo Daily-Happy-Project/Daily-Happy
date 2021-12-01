9b8be94b2a5b2f576fb04d23da7fee42532ac715
ycyeon <ickite428@gmail.com>
2021-12-01 13:59:27 +0900

    랜덤페이퍼 원래대로 수정

--- DailyHappy/src/main/java/dao/UserItemDAO.java
+++ DailyHappy/src/main/java/dao/UserItemDAO.java
@@ -86,41 +86,33 @@ public class UserItemDAO {
 	        }
 	    }
 		
-		
-// random Paper
-	public String randomPaper(String email) throws NamingException, SQLException {
-		Connection conn = ConnectionPool.get();
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		ResultSet rs2 = null;
-		try {
-			email = new UserDAO().splitemail(email);
-			String sql = "SELECT itemCode FROM " + email + "Item WHERE itemType=\"paper\" ORDER BY rand() limit 1";
-			stmt = conn.prepareStatement(sql);
-			rs = stmt.executeQuery();
-			int itemCode=0;
-			String img1="";
-			while(rs.next()) {
-				itemCode=rs.getInt("itemCode");
+
+	
+	// random Paper
+		public ArrayList<UserItemObj> randomPaper(String email) throws NamingException, SQLException {
+			Connection conn = ConnectionPool.get();
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			try {
+				email = new UserDAO().splitemail(email);
+				String sql = "SELECT img1, itemCode FROM " + email + "item WHERE itemType=\"paper\" Order by rand() limit 1";
+				stmt = conn.prepareStatement(sql);
+				rs = stmt.executeQuery();
+
+//				String img = rs.getString("img1");
+//				int itemCode = rs.getInt("itemCode");
+				ArrayList<UserItemObj> items = new ArrayList<UserItemObj>();
+				while(rs.next()) {
+					items.add(new UserItemObj(rs.getInt("itemCode"), rs.getString("img1")));
+				}
+				return items;			
+
+			} finally {
+				if (rs != null) rs.close();
+	            if (stmt != null) stmt.close(); 
+	            if (conn != null) conn.close();
 			}
-			
-			sql = "SELECT img1 FROM item WHERE itemCode=\"" + itemCode +"\"";
-			rs2 = stmt.executeQuery(sql);
-			while(rs2.next()) {
-				img1 = rs2.getString("img1");
-			}
-			
-			return img1;			
-			
-		} finally {
-			if (rs2 != null) rs2.close();
-			if (rs != null) rs.close();
-            if (stmt != null) stmt.close(); 
-            if (conn != null) conn.close();
 		}
-		
-	}
-		
 		
 		
 		
